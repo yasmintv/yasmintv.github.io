8d1e82a97ce29b3d7bed83b87d09848c84effc71
yasmintv <71607465+yasmintv@users.noreply.github.com>
2020-09-20 23:03:07 +0300

    New files
    :3

---
+++ script/site.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+	$("#twitchBtn").mouseover(function () {
+		$("body").css("background-color", "#b481ff");
+	});
+	$("#twitchBtn").mouseleave(function () {
+		$("body").css("background-color", "#ffe4c7");
+	});
+	$("#twitterBtn").mouseover(function () {
+		$("body").css("background-color", "#78c2ef");
+	});
+	$("#twitterBtn").mouseleave(function () {
+		$("body").css("background-color", "#ffe4c7");
+	});
+	$("#instaBtn").mouseover(function () {
+		$("body").css("background-color", "#fbc77f");
+	});
+	$("#instaBtn").mouseleave(function () {
+		$("body").css("background-color", "#ffe4c7");
+	});
+	$("#discordBtn").mouseover(function () {
+		$("body").css("background-color", "#97a6da");
+	});
+	$("#discordBtn").mouseleave(function () {
+		$("body").css("background-color", "#ffe4c7");
+	});
+	$("#ytBtn").mouseover(function () {
+		$("body").css("background-color", "#fd7e7e");
+	});
+	$("#ytBtn").mouseleave(function () {
+		$("body").css("background-color", "#ffe4c7");
+	});
+});
+
+// $("#twitchBtn").mouseover(function () {
+			// 	$("body").css("background-color", "#9146ff");
+			// 	$("#twitchIcon").css({"color": "#9146ff"});
+			// });
+			// $("#twitchBtn").mouseleave(function () {
+			// 	$("body").css("background-color", "#ffe4c7");
+			// 	$("#twitchIcon").css({"color": "#000"});
+			// });
+			// $("#twitterBtn").mouseover(function () {
+			// 	$("body").css("background-color", "#1da1f2");
+			// 	$("#twitterIcon").css({"color": "#1da1f2"});
+			// });
+			// $("#twitterBtn").mouseleave(function () {
+			// 	$("body").css("background-color", "#ffe4c7");
+			// 	$("#twitterIcon").css({"color": "#000"});
+			// });
+			// $("#instaBtn").mouseover(function () {
+			// 	$("body").css("background-color", "#fcaf45");
+			// 	$("#instaIcon").css({"color": "#fcaf45"});
+			// });
+			// $("#instaBtn").mouseleave(function () {
+			// 	$("body").css("background-color", "#ffe4c7");
+			// 	$("#instaIcon").css({"color": "#000"});
+			// });
+			// $("#discordBtn").mouseover(function () {
+			// 	$("body").css("background-color", "#7289da");
+			// 	$("#discordIcon").css({"color": "#7289da"});
+			// });
+			// $("#discordBtn").mouseleave(function () {
+			// 	$("body").css("background-color", "#ffe4c7");
+			// 	$("#discordIcon").css({"color": "#000"});
+			// });
+			// $("#ytBtn").mouseover(function () {
+			// 	$("body").css("background-color", "#ff0000");
+			// 	$("#ytIcon").css({"color": "#ff0000"});
+			// });
+			// $("#ytBtn").mouseleave(function () {
+			// 	$("body").css("background-color", "#ffe4c7");
+			// 	$("#ytIcon").css({"color": "#000"});
+			// });
